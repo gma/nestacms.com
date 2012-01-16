2228de2954568fb7ebc3149b27e9789320219954
Graham Ashton <graham@effectif.com>
2012-01-16 12:15:19 +0000

    Support for attributing authorship to articles.

--- app.rb
+++ app.rb
@@ -1,3 +1,22 @@
 Encoding.default_external = 'utf-8'
 
 Tilt.prefer Tilt::MarukuTemplate
+
+module Nesta
+  class App
+    def author_biography(name = nil)
+      name ||= @page.metadata('author')
+      if name
+        short_name = name.downcase.gsub(/\W+/, '_').to_sym
+        avatar_path = File.join(['images', 'authors', "#{short_name}.jpg"])
+        html = ""
+        if File.exist?(File.join(Nesta::App.root, 'public', avatar_path))
+          html += capture_haml do
+            haml_tag :img, :src => "/#{avatar_path}", :class => 'avatar'
+          end
+        end
+        html << haml(short_name.to_sym, :layout => false)
+      end
+    end
+  end
+end
